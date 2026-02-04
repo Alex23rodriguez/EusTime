3034002214f819736eb206a94cf240eba5293420
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2026-02-04 19:22:32 +0100

    feat: main

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from quizzer import APIGame
+
 from future import FuturePluralQ, FutureSingularQ
 from past import PastPluralQ, PastSingularQ
 from vocab import DaysQ, MonthsQ, SeasonsQ
+
+game = APIGame(
+    "Euskera - Time",
+    {
+        "future_s": FutureSingularQ,
+        "future_p": FuturePluralQ,
+        "past_s": PastSingularQ,
+        "past_p": PastPluralQ,
+        "days": DaysQ,
+        "months": MonthsQ,
+        "seasons": SeasonsQ,
+    },
+)
+
+game.start()
